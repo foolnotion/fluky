b6e61b4525ab4a7977e29cf4625fa9d9166f2134
Bogdan Burlacu <bogdan.burlacu@pm.me>
2025-04-06 12:18:07 +0200

    flake: add aarch64-linux as a supported system

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 
   outputs = inputs @{ self, flake-parts, foolnotion, nixpkgs }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
       perSystem = { pkgs, system, ... }:
         let
